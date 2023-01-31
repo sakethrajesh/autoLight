180aa089d0765b23d78f279952d35a3e63c6d9dd
Saketh <sakethraj122@gmail.com>
2023-01-31 11:17:00 -0500

    update

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def distance(lat1, lat2, lon1, lon2):
     c = 2 * asin(sqrt(a))
     
     # Radius of earth in kilometers. Use 3956 for miles
-    r = 3956*5280
+    r = c*20925721.784777
       
     # calculate the result
-    return(c * r)
+    return r
 
 email = input('Email: ')
 password = getpass.getpass("Password: ")
@@ -39,7 +39,10 @@ print(api.data)
 now = datetime.datetime.now()
 print('time now ' + str(now))
 six_pm = now.replace(hour=18, minute=0, second=0, microsecond=0)
+six_am = now.replace(hour=6, minute=0, second=0, microsecond=0)
 print(now > six_pm)
+print(now < six_am)
+
 
 url = 'https://developer-api.govee.com/v1'
 headers = {"content-type": "application/json", 'Govee-API-Key': govee_key }
@@ -51,20 +54,24 @@ payload = {
         'value': 'off'
     }
 }
-print(requests.put(url + '/devices/control', data=json.dumps(payload), headers=headers).content)
+requests.put(url + '/devices/control', data=json.dumps(payload), headers=headers)
 status = False
 off = 0;
 
-while off < 100:
+while True:
     latitude = api.devices[3].location()['latitude']
     longitude = api.devices[3].location()['longitude']
-    # print('coordinates: (' + str(latitude) + '', '' + str(longitude) + ')')
+    print('coordinates: (' + str(latitude) + '', '' + str(longitude) + ')')
 
+    # distance = distance(lat1=37.24243167545431, lon1=-80.42904638540682, lat2=latitude, lon2=longitude)
+    print(distance(37.24243167545431, latitude, -80.42904638540682, longitude))
     print('**************************************************************')
 
     off += 1
 
-    if now <= six_pm and distance(lat1=latitude, lon1=longitude, lat2=37.683441657148066, lon2=-120.94261014871186) <= 8:
+    # 
+
+    if (now >= six_pm or now <= six_am) and distance(37.24243167545431, latitude, -80.42904638540682, longitude) <= 100:
         print('in range')
 
         if not status:
@@ -85,6 +92,22 @@ while off < 100:
 
     else:
         print('not in range')
+
+        if status:
+            payload = {
+                'device':'e1:66:34:20:03:6d:62:62',
+                'model':'H5081',
+                'cmd': {
+                    'name': 'turn',
+                    'value': 'off'
+                }
+            }
+
+            print(requests.put(url + '/devices/control', data=json.dumps(payload), headers=headers).content)
+            status = False
+        else:
+            print('already off') 
+
 
     # if input('end service (yes/no): ') == 'yes':
     #     off = True
